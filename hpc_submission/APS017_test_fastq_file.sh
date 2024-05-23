0a63902528d3974583dd3407fc40ab99f5d28d8f
apsteinberg <asherps@gmail.com>
2024-05-23 15:05:52 -0400

    MSK commits

--- hpc_submission/APS017_test_fastq_file.sh
+++ hpc_submission/APS017_test_fastq_file.sh
@@ -22,10 +22,10 @@ fastqdir=${outdir}/wgs_test_fastq
 
 mkdir -p ${fastqdir}
 
-input_fastq=/data1/shahs3/isabl_data_lake/experiments/12/78/21278/data/SHAH_H003460_T01_02_WG01_188920f778ab35bb_XPRO_0287_T_DNA_IGO_08822_GY_1_S73_R1_001.fastq.gz
-output_fastq=${fastqdir}/TCDO-SAR-034-PDX_R1.downsampled.fastq.gz
-seqtk sample -s100 ${input_fastq} 0.01 | gzip > ${output_fastq}
+#input_fastq=/data1/shahs3/isabl_data_lake/experiments/12/78/21278/data/SHAH_H003460_T01_02_WG01_188920f778ab35bb_XPRO_0287_T_DNA_IGO_08822_GY_1_S73_R1_001.fastq.gz
+#output_fastq=${fastqdir}/TCDO-SAR-034-PDX_R1.downsampled.fastq.gz
+#seqtk sample -s100 ${input_fastq} 0.01 | gzip > ${output_fastq}
 ## second fastq file ....
-input_fastq=/data1/shahs3/isabl_data_lake/experiments/12/78/21278/data/SHAH_H003460_T01_02_WG01_188920f778ab35bb_XPRO_0287_T_DNA_IGO_08822_GY_1_S73_R2_001.fastq.g
+input_fastq=/data1/shahs3/isabl_data_lake/experiments/12/78/21278/data/SHAH_H003460_T01_02_WG01_188920f778ab35bb_XPRO_0287_T_DNA_IGO_08822_GY_1_S73_R2_001.fastq.gz
 output_fastq=${fastqdir}/TCDO-SAR-034-PDX_R2.downsampled.fastq.gz
 seqtk sample -s100 ${input_fastq} 0.01 | gzip > ${output_fastq}
